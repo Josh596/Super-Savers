584be4291578444ba774251c3f205dd5d7065d53
Josh596 <joshuaolukotun3@gmail.com>
2021-10-09 11:33:14 +0100

    Created new app basket

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
-]
+    path('', include('store.urls')),
+    path("basket/", include("basket.urls", namespace="basket"))
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
