a9ee78af28d6ff7a3828ff8406a1e6e7fddf660f
Josh596 <joshuaolukotun3@gmail.com>
2021-10-22 00:47:25 +0100

    Minor change

--- config/urls.py
+++ config/urls.py
@@ -30,7 +30,3 @@ urlpatterns = [
     path('farmer/', include("vendor.urls", namespace="vendors"))
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
