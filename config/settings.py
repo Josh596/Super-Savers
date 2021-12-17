ad904c5a285aa0921a1a198b9f3a65caadf86d85
Josh596 <joshuaolukotun3@gmail.com>
2021-12-17 12:54:32 +0100

    Minor

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x&eu8)b5@b^gan*-dqtn9xgrq^=^iupx9oo8b9i)phgdr8@gpd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['supersavers.herokuapp.com', '127.0.0.1']
 
